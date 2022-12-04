e2e203f3d4676200ef74fec31fe9e328bd06126e
Farzin khaledi <Khaledifarzin1@gmail.com>
2022-12-04 19:05:29 +0000

    uploding the data from database and render them

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,26 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card';
 import './CardStyle.css';
-// import db from './firebase';
-// import { collection, getDocs } from '@firebase/firestore';
-
-const data = [
-  {
-    name: 'jack',
-    url: 'https://faroutmagazine.co.uk/static/uploads/2022/03/Titanic-Jack-1.jpg',
-  },
-  {
-    name: 'jenifer',
-    url: 'https://media.vanityfair.com/photos/618c0614fdf674f0522a9fb7/master/pass/LBY_210805_JENNIFER_LAWRENCE_VF_09G_Shot_10_028_QC_sRGB_LR.jpg',
-  },
-  {
-    name: 'mike',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/e/e1/Mike_Tyson_2019_by_Glenn_Francis.jpg',
-  },
-];
+import db from './firebase';
+import { collection, onSnapshot } from 'firebase/firestore';
+// const data = [
+//   {
+//     name: 'jack',
+//     url: 'https://faroutmagazine.co.uk/static/uploads/2022/03/Titanic-Jack-1.jpg',
+//   },
+//   {
+//     name: 'jenifer',
+//     url: 'https://media.vanityfair.com/photos/618c0614fdf674f0522a9fb7/master/pass/LBY_210805_JENNIFER_LAWRENCE_VF_09G_Shot_10_028_QC_sRGB_LR.jpg',
+//   },
+//   {
+//     name: 'mike',
+//     url: 'https://upload.wikimedia.org/wikipedia/commons/e/e1/Mike_Tyson_2019_by_Glenn_Francis.jpg',
+//   },
+// ];
 
 function TinderCards() {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState([]);
+
+
+  useEffect(() => {
+    onSnapshot(
+      collection(db, 'people'),
+      (snapShot) => {
+        setPeople(snapShot.docs.map((doc) => doc.data()));
+      },
+      (error) => {
+        throw error;
+      }
+    );
+  }, []);
+
+  console.log(people);
 
   return (
     <div>
@@ -46,23 +60,19 @@ function TinderCards() {
 }
 
 export default TinderCards;
-// const peopleCollectionRef = collection(firestore, 'people');
-
-// useEffect(() => {
-//   const getPeople = async () => {
-//     const data = await getDocs(peopleCollectionRef);
-//     setPeople(data.Docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-//   };
-//   getPeople();
-// }, [people]);
 // setPeople(data);
 // console.log(people, 'innnnnnnn');
-// useEffect(() => {
-//   firestore
-//     .collection('people')
-//     .onSnapshot((snapShot) =>
-//       setPeople(snapShot.docs.map((doc) => doc.data()))
-//     );
-// }, [people]);
 
-// console.log(people);
+// const peopleCollectionRef = db.collection('people').get();
+// console.log(peopleCollectionRef);
+
+// console.log(peopleCollectionRef);
+// useEffect(() => {
+//   const getPeople = async () => {
+//     const data = await getDoc(peopleCollectionRef);
+//     console.log(data.Doc);
+//     setPeople(data.Doc.map((doc) => ({ ...doc.data(), id: doc.id })));
+//     console.log(data.Doc);
+//   };
+//   getPeople();
+// }, []);
